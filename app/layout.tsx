2a0eec171f670b2d93245f911985396e11ad33d6
takayums <takayums20@gmail.com>
2024-06-26 12:01:00 +0700

    feat(blog): add mdx for markdown blog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import { roboto } from "./fonts";
 import "./global.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -7,7 +8,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <title>Nextjs Headless CMS</title>
       </head>
-      <body className="flex flex-col min-h-screen">
+      <body className={`flex flex-col min-h-screen ${roboto.className}`}>
         <Navbar />
         <main className="grow">{children}</main>
         <footer className="border-t text-center text-sm p-3">
